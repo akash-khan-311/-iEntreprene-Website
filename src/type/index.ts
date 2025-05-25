ec42de579d8c76366e3d9e9b0162462c0d8f6619
Akash Khan <mdakashkhanbdinto@gmail.com>
2025-05-25 19:44:00 +0600

    project initilized

---
+++ src/type/index.ts
@@ -0,0 +1,20 @@
+export type AnimationSnapshot = {
+  filter?: string;
+  opacity?: number;
+  y?: number;
+};
+
+export type BlurTextProps = {
+  text?: string;
+  delay?: number;
+  className?: string;
+  animateBy?: 'words' | 'letters';
+  direction?: 'top' | 'bottom';
+  threshold?: number;
+  rootMargin?: string;
+  animationFrom?: AnimationSnapshot;
+  animationTo?: AnimationSnapshot[];
+  easing?: (t: number) => number;
+  onAnimationComplete?: () => void;
+  stepDuration?: number;
+};
